d7a832c0a9cf467371be454055145ed0eb8e60a7
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2025-04-11 15:42:17 +0200

    Update footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
 	return (
 		<main className="flex flex-col items-center justify-center h-screen w-screen gap-8">
 			<Main />
+      <div className="flex justify-center items-center">
+			<img src="/logo.svg" alt="Logo The Archivist" className="w-10 h-10" />
+			</div>
 			<Footer />
 		</main>
 	);
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import { FaGithub, FaUserCircle } from 'react-icons/fa';
 
+const year = new Date().getFullYear();
+
 const Footer = () => {
     return (
         <footer className=" absolute bottom-2 w-full flex justify-between items-center text-sm">
@@ -10,9 +12,7 @@ const Footer = () => {
                     <FaUserCircle className="size-6" />
                 </Link>
             </p>
-            <div className="flex justify-center items-center">
-				<img src="/logo.svg" alt="Logo The Archivist" className="w-10 h-10" />
-			</div>
+            <p className="uppercase tracking-wider text-[0.75rem] text-muted-foreground">Kevin Bourgitteau - {year}</p>
             <p>
                 <Link href="https://github.com/ka-be/thearchivist" target="_blank">
                     <FaGithub className="size-6" />
